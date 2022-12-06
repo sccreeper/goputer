1b24f7f96892427c598ed6645025cfaa0d8c146c
Oscar Peace <oscar@oscarcp.net>
2022-12-06 19:48:12 +0000

    Remove unneeded files from build

--- frontends/goputerpy/cmd.sh
+++ frontends/goputerpy/cmd.sh
@@ -7,6 +7,7 @@ mkdir ./build/goputerpy
 
 go build -buildmode=plugin -ldflags "-s -w" -o ./build/goputerpy.so ./cli_interface.go
 go build -buildmode=c-shared -ldflags "-s -w" -o ./build/goputerpy/bindings.so ./goputerpy/bindings.go
+rm ./build/goputerpy/bindings.h
 
-cp -r ./goputerpy/* ./build/goputerpy/
+find ./goputerpy -name "*.py" -exec cp -prv "{}" "./build/goputerpy" ";"
 cp ./main.py ./build/main.py
