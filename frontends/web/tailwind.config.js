fd079a327008cc78e38afc60c33939510be052db
Oscar Peace <oscar@oscarcp.net>
2022-12-13 22:23:06 +0000

    Initial CSS for WASM frontend

---
+++ frontends/web/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
