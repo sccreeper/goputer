b9b55dd06d6fe9d7d6adf9ebe836c9d08380eab2
Oscar Peace <oscar@oscarcp.net>
2025-08-13 11:25:28 +0100

    feat: python frontend as standalone

--- frontends/goputerpy/goputerpy/util.py
+++ frontends/goputerpy/goputerpy/util.py
@@ -6,29 +6,5 @@ def convert_colour(c: int) -> pg.Color:
 
     return pg.Color(c[0], c[1], c[2], a=c[3])
 
-def generate_instruction_string(itn: int, large_arg: int, args: list[2]) -> str:
-
-    if itn + large_arg + args[0] + args[1] == 0:
-        return "Finished"
-
-
-    itn_string = ""
-
-    itn_string += c.InstructionStrings[itn]
-
-    if c.InstructionArgCounts[itn] == 1:
-        
-        if itn == c.Instructions["jmp"] or itn == c.Instructions["cndjmp"] or itn == c.Instructions["call"] or itn == c.Instructions["cndcall"] or itn == c.Instructions["lda"] or itn == c.Instructions["sta"]:
-            itn_string += f" {convert_to_hex(large_arg)}"
-        elif itn == c.Instructions["int"]:
-            itn_string += f" {c.InterruptStrings[large_arg]}"
-        else:
-            itn_string += f" {c.RegisterStrings[large_arg]}"
-    
-    elif c.InstructionArgCounts[itn] == 2: 
-        itn_string += f" {c.RegisterStrings[args[0]]} {c.RegisterStrings[args[1]]}"
-    
-    return itn_string
-
 def convert_to_hex(i: int) -> str:
     return f"0x{i:0>8X}"
