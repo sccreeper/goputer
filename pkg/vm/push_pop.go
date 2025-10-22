a129829f7478eae10ba1a5f64ddbb40957e7268c
Oscar Peace <oscar@oscarcp.net>
2025-10-22 19:00:22 +0100

    fix: data stack
    tests: add push pop test for registers

--- pkg/vm/push_pop.go
+++ pkg/vm/push_pop.go
@@ -7,27 +7,27 @@ import (
 )
 
 func (m *VM) popStack() {
-	if !(m.Registers[c.RStackPointer]-4 < m.Registers[c.RStackZeroPointer]) {
+	if m.Registers[c.RStackPointer]-4 >= m.Registers[c.RStackZeroPointer] {
+		m.Registers[m.LeftArg] =
+			binary.LittleEndian.Uint32(
+				m.MemArray[m.Registers[c.RStackPointer]-4 : m.Registers[c.RStackPointer]],
+			)
+
 		m.Registers[c.RStackPointer] -= 4
 	} else {
 		m.Registers[c.RStackPointer] = m.Registers[c.RStackZeroPointer] // prevent underflow
 	}
 
-	m.Registers[m.LeftArg] =
-		binary.LittleEndian.Uint32(
-			m.MemArray[m.Registers[c.RStackPointer]-4 : m.Registers[c.RStackPointer]],
-		)
-
 }
 
 func (m *VM) pushStack() {
 
-	binary.LittleEndian.PutUint32(
-		m.MemArray[m.Registers[c.RStackPointer]:m.Registers[c.RStackPointer]+4],
-		uint32(m.LeftArgVal),
-	)
+	if m.Registers[c.RStackPointer]+4 < compiler.MemOffset {
+		binary.LittleEndian.PutUint32(
+			m.MemArray[m.Registers[c.RStackPointer]:m.Registers[c.RStackPointer]+4],
+			m.LeftArgVal,
+		)
 
-	if m.Registers[c.RStackPointer]+4 < compiler.DataStackSize+VideoBufferSize {
 		m.Registers[c.RStackPointer] += 4
 	} else {
 		panic("data stack overflow")
